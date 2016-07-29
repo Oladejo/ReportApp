a56ea7c54f704ce54b0fc45daca49367c2b883ca
Oladejo Azeez <bab.oladejo@student.oauife.edu.ng>
2016-07-29 22:14:54 +0100

    leader authorization handler

---
+++ ReportApp.Web/CustomAuthorization/AuthorizationHandlers.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+
+namespace ReportApp.Web.CustomAuthorization
+{
+    public class ClaimAuthorization : AuthorizeAttribute
+    {
+        public string View { get; set; }
+
+        public ClaimAuthorization()
+        {
+            View = "AuthorizedFailed";
+        }
+
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            base.OnAuthorization(filterContext);
+            IsUserAuthorized(filterContext);
+        }
+
+        private void IsUserAuthorized(AuthorizationContext filterContext)
+        {
+            if (filterContext.Result == null && (filterContext.HttpContext.User.IsInRole("Unit")
+                || filterContext.HttpContext.User.IsInRole("Department")))
+                return;
+
+            if (filterContext.HttpContext.User.Identity.IsAuthenticated)
+            {   
+                var vr = new ViewResult {ViewName = View};
+                var dict = new ViewDataDictionary { { "Message", "Sorry you are not Authorized to Perform this Action" } };
+                vr.ViewData = dict;
+                var result = vr;
+                filterContext.Result = result;
+            }
+        }
+    }
+}
