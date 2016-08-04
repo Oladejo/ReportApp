ecac3e1ced59966a5b8adee1db15988268fd8e7c
Oladejo Azeez <bab.oladejo@student.oauife.edu.ng>
2016-08-04 09:54:33 +0100

    custom edit profile view model

--- ReportApp.Web/Models/CustomModel.cs
+++ ReportApp.Web/Models/CustomModel.cs
@@ -1,13 +1,10 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Web.Mvc;
+using ReportApp.Core.Entities;
 
 namespace ReportApp.Web.Models
 {
-    public class CustomModel
-    {
-    }
-
     public class RoleViewModel
     {
         public string Id { get; set; }
@@ -27,6 +24,37 @@ namespace ReportApp.Web.Models
         public string Unit { get; set; }
 
         public string Department { get; set; }
+
+        public IEnumerable<SelectListItem> RolesList { get; set; }
+    }
+
+    public class StaffProfile
+    {
+        [Required]
+        [Display(Name = "Full Name")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        public string FullName { get; set; }
+
+        [Required]
+        [Display(Name = "Department")]
+        public int DepartmentId { get; set; }
+
+        [Required]
+        [Display(Name = "Unit")]
+        public int UnitId { get; set; }
+
+        [Required]
+        [Display(Name = "Gender")]
+        public Gender Gender { get; set; }
+
+        [Required]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
 
         public IEnumerable<SelectListItem> RolesList { get; set; }
     }
