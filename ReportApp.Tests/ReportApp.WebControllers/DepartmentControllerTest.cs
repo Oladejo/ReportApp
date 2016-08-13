e78e8295c057cc12e1224e803fd4e753fbccd54e
Oladejo Azeez <bab.oladejo@student.oauife.edu.ng>
2016-08-13 12:32:39 +0100

    Add Unit test project

---
+++ ReportApp.Tests/ReportApp.WebControllers/DepartmentControllerTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Moq;
+using ReportApp.Web.Controllers;
+using NUnit.Framework;
+using ReportApp.Core.Abstract;
+using ReportApp.Core.Entities;
+
+namespace ReportApp.Tests.ReportApp.WebControllers
+{
+    [TestFixture]
+    public class DepartmentControllerTest
+    {
+        [Test]
+        public void DepartmentIndexViewContainListOfDepartment()
+        {
+            //Arrange
+            Mock<IDepartment> mock = new Mock<IDepartment>();
+
+            mock.Setup(m => m.GetDepartments()).Returns(new Department[]
+            {
+                new Department { DepartmentId = 1, DepartmentName = "Account"},
+                new Department { DepartmentId = 2, DepartmentName = "Sales"} 
+            }.AsEnumerable());
+
+            DepartmentsController departments = new DepartmentsController(mock.Object);
+
+            //Act
+            var actual = (List<Department>) departments.Index().Model;
+
+            //Assert
+            Assert.IsInstanceOf<List<Department>>(actual);
+        }
+    }
+}
