483eded573bac6fe35a9229a66c52ad294311b2b
Oladejo Azeez <bab.oladejo@student.oauife.edu.ng>
2016-10-05 06:34:14 +0100

    report search and sorting for staff

--- ReportApp.Web/Controllers/StaffController.cs
+++ ReportApp.Web/Controllers/StaffController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
+using PagedList;
 using ReportApp.Core.Abstract;
 using ReportApp.Core.Concrete;
 using ReportApp.Core.Entities;
@@ -42,11 +43,71 @@ namespace ReportApp.Web.Controllers
             return profile;
         }
 
-        public ActionResult Report()
+        public ActionResult Report(string sortOrder, string searchString, string currentFilter, int? page)
         {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.TitleSort = sortOrder == "title" ? "title_desc" : "title";
+            ViewBag.reportDateSort = sortOrder == "reportDate" ? "reportDate_desc" : "reportDate";
+            ViewBag.submissionDateSort = String.IsNullOrEmpty(sortOrder) ? "submissionDate_desc" : "";
+            ViewBag.typeSort = sortOrder == "type" ? "type_desc" : "type";
+
+            //use in pagination process
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
             var profile = GetProfile();
-            var reports = _reportRepository.GetReport().Where(x => x.Profile.Id == profile.Id).ToList();
-            return View(reports);
+            var reports = _reportRepository.GetReport().Where(x => x.Profile.Id == profile.Id);
+
+            //use for searching
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                //search only by title or report content
+                reports = reports.Where(s => s.ReportContent.Contains(searchString) || s.Title.Contains(searchString)).OrderByDescending(s => s.SubmissionDate);
+            }
+
+            //use for sorting
+            switch (sortOrder)
+            {
+                case "title":
+                    reports = reports.OrderBy(s => s.Title);
+                    break;
+                case "title_desc":
+                    reports = reports.OrderByDescending(s => s.Title);
+                    break;
+                case "reportDate":
+                    reports = reports.OrderBy(s => s.ReportDate);
+                    break;
+                case "reportDate_desc":
+                    reports = reports.OrderByDescending(s => s.ReportDate);
+                    break;
+                case "submissionDate_desc":
+                    reports = reports.OrderBy(s => s.SubmissionDate);
+                    break;
+                case "type_desc":
+                    reports = reports.OrderByDescending(s => s.ReportType);
+                    break;
+                case "type":
+                    reports = reports.OrderBy(s => s.ReportType);
+                    break;
+                default:
+                    reports = reports.OrderByDescending(s => s.SubmissionDate);
+                    break;
+            }
+
+            const int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            return View(reports.ToPagedList(pageNumber, pageSize));
+
+            //var profile = GetProfile();
+            //var reports = _reportRepository.GetReport().Where(x => x.Profile.Id == profile.Id).ToList();
+            //return View(reports);
         }
 
         public ActionResult ReportDetails(string id)
